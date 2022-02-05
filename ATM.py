c8056526973115a4e5be2df791dd36af280d9316
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2022-02-05 14:45:29 +0530

    new program files

--- ATM.py
+++ ATM.py
@@ -6,26 +6,38 @@ class Atm(object):
     
     def withdraw(self,ammount):
         if self.balance >= ammount:
-            self.balance = self.balance - ammount
-            print("ammount debited :"+ str(ammount))
-            print("balance remaining :"+str(self.balance))
+            pin = int(input("Enter card pin number to withdraw:"))
+            if pin == int(self.pinNum):
+                self.balance = self.balance - ammount
+                print("ammount debited :"+ str(ammount))
+                print("balance remaining :"+str(self.balance))
+            elif pin != int(self.pinNum):
+                print("Wrong Pin ")
         else :
             print("You dont have enough balance!!")
 
     def checkBalance(self):
-        print(str(self.balance))
-
+        pin = int(input("Enter card pin number to check balance:"))
+        if pin == int(self.pinNum):
+            print(str(self.balance))
+        elif pin != int(self.pinNum):
+            print("Wrong Pin")
+           
     def depositMoney(self,ammount):
-        self.balance = self.balance + ammount
-        print("ammount deposited :"+ str(ammount))
-        print("New balance :"+str(self.balance))
+        pin = int(input("Enter card pin number to deposit:"))
+        if pin == int(self.pinNum):
+            self.balance = self.balance + ammount
+            print("ammount deposited :"+ str(ammount))
+            print("New balance :"+str(self.balance))
+        elif pin != int(self.pinNum):
+            print("Wrong Pin ")
 
 # creating object from the class
 Mohan = Atm(224422 , 885588 , 5000)
 Liz = Atm(442221 , 445544 , 8000)
 
-#Using Functions
+#Using Functions use pins Mohan = 885588 and Liz = 445544
 Mohan.checkBalance()
-Liz.withdraw(5000)
-Mohan.withdraw(10000)
-Mohan.depositMoney(10000)
+#Liz.withdraw(5000) 
+#Mohan.withdraw(10000)
+#Mohan.depositMoney(10000)
